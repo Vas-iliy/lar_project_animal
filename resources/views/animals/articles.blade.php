77278a5ecabb7f93d90ce9740e9171393c239f5b
Vasiliy <kkolyasev1228@gmail.com>
2020-10-01 21:24:52 +0300

    site

---
+++ resources/views/animals/articles.blade.php
@@ -0,0 +1,49 @@
+<section class="section blog">
+    <div class="container">
+
+        <div class="row justify-content-center mb-5 element-animate">
+            <div class="col-md-8 text-center">
+                <h2 class=" heading mb-4">Recent Blog Post</h2>
+                <p class="mb-5 lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi unde impedit, necessitatibus, soluta sit quam minima expedita atque corrupti reiciendis.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <div class="media mb-4 d-md-flex d-block element-animate">
+                    <a href="#" class="mr-5"><img src="img/blog_1.jpg" alt="Placeholder image" class="img-fluid"></a>
+                    <div class="media-body">
+                        <span class="post-meta">Feb 26th, 2018</span>
+                        <h3 class="mt-2 text-black"><a href="#">How to Train Your Dog</a></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam minus ipsa earum nemo consectetur cupiditate necessitatibus suscipit assumenda perspiciatis provident.</p>
+                        <p><a href="#" class="btn btn-primary btn-sm">Read more</a></p>
+                    </div>
+                </div>
+
+
+
+            </div>
+            <div class="col-md-6">
+                <div class="media mb-4 d-md-flex d-block element-animate">
+                    <a href="#" class="mr-5"><img src="img/blog_2.jpg" alt="Placeholder image" class="img-fluid"></a>
+                    <div class="media-body">
+                        <span class="post-meta">Feb 26th, 2018</span>
+                        <h3 class="mt-2 text-black"><a href="#">Find The Right Food For Your Dogs</a></h3>
+                        <p><a href="#" class="btn btn-primary btn-sm">Read more</a></p>
+                    </div>
+                </div>
+
+                <div class="media mb-4 d-md-flex d-block element-animate">
+                    <a href="#" class="mr-5"><img src="img/blog_3.jpg" alt="Placeholder image" class="img-fluid"></a>
+                    <div class="media-body">
+                        <span class="post-meta">Feb 26th, 2018</span>
+                        <h3 class="mt-2 text-black"><a href="#">Dog's Affections To Owner</a></h3>
+                        <p><a href="#" class="btn btn-primary btn-sm">Read more</a></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
